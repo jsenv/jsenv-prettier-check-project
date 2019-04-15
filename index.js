5ac21a118329a79bff6a678b27df4a94de506f3d
Damien Maillard <damien.maillard@dailymotion.com>
2019-04-15 16:45:17 +0200

    fix extendDefaultPrettifyMetaMap

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ import { DEFAULT_PRETTIFY_META_MAP } from "./src/prettier-check-project-constant
 export { prettierCheckProject } from "./src/prettierCheckProject.js"
 
 export const extendDefaultPrettifyMetaMap = ({ prettifyMetaMap }) => {
-  return { ...DEFAULT_PRETTIFY_META_MAP, prettifyMetaMap }
+  return { ...DEFAULT_PRETTIFY_META_MAP, ...prettifyMetaMap }
 }
