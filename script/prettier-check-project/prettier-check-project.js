09b593960addfe65bb873a6740cbfd759494fb4b
Damien Maillard <damien.maillard@dailymotion.com>
2019-05-20 14:15:15 +0200

    prettier check ception

--- script/prettier-check-project/prettier-check-project.js
+++ script/prettier-check-project/prettier-check-project.js
@@ -1,28 +1,10 @@
-const { prettierCheckProject } = require("@jsenv/prettier-check-project")
+const { prettierCheckProject, jsenvPrettifyMap } = require("@jsenv/prettier-check-project")
 const { projectPath } = require("../../jsenv.config.js")
 
 prettierCheckProject({
-  projectFolder: projectPath,
-  prettifyMetaMap: {
-    // js
-    "/index.js": true,
-    "/src/**/*.js": true,
-    "/test/**/*.js": true,
-    "/script/**/*.js": true,
-    "/**/**syntax-error**.js": false,
-    "/**/.dist/": false,
-    "/**/dist/": false,
-    // json
-    "/src/**/*.json": true,
-    "/test/**/*.json": true,
-    "/script/**/*.json": true,
-    // md
-    "/readme.md": true,
-    "/doc/**.md": true,
-    "/src/**/*.md": true,
-    "/test/**/*.md": true,
-    "/script/**/*.md": true,
-
+  projectPath,
+  prettifyMap: {
+    ...jsenvPrettifyMap,
     "/test/basic/errored.js": false,
     "/test/basic/ugly.js": false,
   },
