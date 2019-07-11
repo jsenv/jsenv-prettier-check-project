cea78277346aefb9e3e33085691ea8cc30d786c1
Damien Maillard <dmaillard06@gmail.com>
2019-07-11 23:19:28 +0200

    update latest dependencies, should fix windows

--- script/generate-coverage/generate-coverage.js
+++ script/generate-coverage/generate-coverage.js
@@ -1,6 +1,9 @@
-const { cover } = require("@jsenv/core")
-const { projectPath } = require("../../jsenv.config.js")
+const { cover } = require("@jsenv/testing")
+const { projectPath, testDescription } = require("../../jsenv.config.js")
 
 cover({
   projectPath,
+  executeDescription: testDescription,
+  logCoverageTable: true,
+  writeCoverageHtmlFolder: true,
 })
