b0060b5591eb87eac4de8df6a26b76368860d8bc
Damien Maillard <damien.maillard@dailymotion.com>
2019-12-19 15:06:13 +0100

    update workflow

--- .github/auto-release-on-github.js
+++
@@ -1,7 +0,0 @@
-const { fileURLToPath } = require("url")
-const { autoReleaseOnGithub } = require("@jsenv/auto-publish")
-const { projectDirectoryUrl } = require("../jsenv.config.js")
-
-autoReleaseOnGithub({
-  projectPath: fileURLToPath(projectDirectoryUrl),
-})
