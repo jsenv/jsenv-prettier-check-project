9955d7b1f6dd44c77689bfa3ac85011fa9254cfe
Damien Maillard <damien.maillard@dailymotion.com>
2020-01-17 11:58:35 +0100

    5.2.1
    - fix github directory name

--- src/jsenvProjectFilesConfig.js
+++ src/jsenvProjectFilesConfig.js
@@ -1,5 +1,21 @@
+/**
+ * This object decribes th usual files structure used in jsenv projects.
+ *
+ * It means files inside .github/, docs/, src/, test/ and root files
+ * will be formatted with prettier when supported.
+ *
+ * If a project has an other directory containing files that should be formatted
+ * by prettier it can be added by doing
+ *
+ * {
+ *   ...jsenvProjectFilesConfig,
+ *   './directory/': true
+ * }
+ *
+ */
+
 export const jsenvProjectFilesConfig = {
-  "./github/": true,
+  "./.github/": true,
   "./docs/": true,
   "./src/": true,
   "./test/": true,
